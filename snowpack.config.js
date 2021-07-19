d6c76f71d8d6e9c9870e2765e30b2f7f3d701e45
TBA <tba@ashampoo.com>
2021-07-19 12:39:59 +0200

    setup

---
+++ snowpack.config.js
@@ -0,0 +1,13 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    src: { url: '/', static: true },
+  },
+  buildOptions: {
+    baseUrl: '/cobi-banner',
+    out: './build',
+  },
+}
